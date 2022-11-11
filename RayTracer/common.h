c21dab570a2750ca876d649eb12ce5502ddfbea4
Robert Ward <robertwesleyward2019@gmail.com>
2022-11-11 15:16:08 -0600

    Add project files.

---
+++ RayTracer/common.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <cmath>
+#include <limits>
+#include <memory>
+#include "ray.h"
+#include "vector.h"
+
+using std::nullptr_t;
+using std::make_shared;
+using std::sqrt;
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+
+inline double random_double() {
+    return rand() / (RAND_MAX + 1);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
+}
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common Headers
