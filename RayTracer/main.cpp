b6fa0232bf24427b8faee0f5abb824ed5d589b67
Robert W <robertwesleyward2019@gmail.com>
2022-11-16 21:05:18 -0600

    Added boxes and Paralleized some of the drawing

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -1,6 +1,8 @@
 #pragma once
-#include "common.h"
 #include <iostream>
+#include <future>
+#include <chrono>
+#include "common.h"
 #include "camera.h"
 #include "color.h"
 #include "hittable_list.h"
@@ -9,41 +11,70 @@
 #include "material.h"
 #include "bvh.h"
 #include "xy_rect.h"
-color ray_color(const ray& r,  const hittable& world, int depth) {
+#include "yz_rect.h"
+#include "xz_rect.h"
+color ray_color(const ray& r,color& bg , const hittable& world, int depth) {
     hit_record rec;
 
+    // If we've exceeded the ray bounce limit, no more light is gathered.
     if (depth <= 0)
         return color(0, 0, 0);
 
-    if (world.hit(r, 0.001, infinity, rec)) {
-        ray scattered;
-        color attenuation;
-        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation * ray_color(scattered, world, depth - 1);
-        return color(0, 0, 0);
+    // If the ray hits nothing, return the background color.
+    if (!world.hit(r, 0.001, infinity, rec))
+        return bg;
+
+    ray scattered;
+    color attenuation;
+    color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+
+    if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+        return emitted;
+
+    return emitted + attenuation * ray_color(scattered, bg, world, depth - 1);
+}
+
+hittable_list createBox() {
+    hittable_list objects;
+
+    auto red = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    auto green = make_shared<lambertian>(color(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    return objects;
+
+}
+
+
+static color getColor(
+    int i,int j,int samples_per_pixel,int image_width,int image_height,
+    camera cam,color background,hittable_list world, int max_depth) {
+
+    color pixel_color(0, 0, 0);
+    for (int s = 0; s < samples_per_pixel; ++s) {
+        auto u = (i + random_double()) / (image_width - 1);
+        auto v = (j + random_double()) / (image_height - 1);
+        ray r = cam.get_ray(u, v);
+        pixel_color += ray_color(r, background, world, max_depth);
     }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
-
-   // ray scattered;
-   // color attenuation;
-   // color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
-
-    //if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-        //return emitted;
-
-    //return emitted + attenuation * ray_color(scattered, world, depth - 1);
+    return pixel_color;
 }
 
 hittable_list random_scene() {
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
 
-    for (int a = -11; a < 11; a++) {
+    for (int a = -11; a < 1; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
             point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
@@ -85,7 +116,7 @@ hittable_list simple_light() {
 
     return objects;
 }
-
+std::vector<std::future<color>> futures;
 int main() {
 
     // Image
@@ -102,7 +133,7 @@ int main() {
     auto aperture = 0.1;
     int image_height = static_cast<int>(image_width / aspect_ratio);
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
-
+    color background(0, 0, 0);
     // World
     hittable_list world;
     auto vfov = 40.0;
@@ -110,27 +141,37 @@ int main() {
     switch (1) {
     case 1:
         world = random_scene();
+        background = color(0.7, 0.8, 1.0);
         lookfrom = point3(13, 2, 3);
         lookat = point3(0, 0, 0);
         vfov = 20.0;
         aperture = 0.1;
         break;
-
-    default:
     case 2:
         world = simple_light();
         lookfrom = point3(13, 2, 3);
         lookat = point3(0, 0, 0);
         vfov = 20.0;
         break;
+    case 3:
+        world = createBox();
+        aspect_ratio = 1.0;
+        image_width = 600;
+        samples_per_pixel = 200;
+        background = color(0, 0, 0);
+        lookfrom = point3(278, 278, -800);
+        lookat = point3(278, 278, 0);
+        vfov = 40.0;
+        break;
     }
 
-
+    
 
 
     // Render
+    auto t1 = std::chrono::high_resolution_clock::now();
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
+    std::vector<color>* colors =  new std::vector<color>();
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' '<< std::flush;
         for (int i = 0; i < image_width; ++i) {
@@ -139,11 +180,18 @@ int main() {
                 auto u = (i + random_double()) / (image_width - 1);
                 auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world,max_depth);
+                pixel_color += ray_color(r, background, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
+             //futures.push_back(std::async(std::launch::async, getColor,i,j,samples_per_pixel,image_width,image_height,cam,background,world,max_depth));
         }
-
     }
+    /*for (auto& future: futures)
+    {
+        write_color(std::cout, future.get(), 50);
+    }*/
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto mSec = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
     std::cerr << "\nDone.\n";
+    std::cerr << "Elapsed time: " << mSec.count() << " ms\n";
 }
--- RayTracer/xy_rect.h
+++ RayTracer/xy_rect.h
@@ -1,4 +1,3 @@
-#include "common.h"
 #include "hittable.h"
 
 class xy_rect:public hittable
